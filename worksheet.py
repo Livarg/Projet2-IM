9092296244ab0a96a75cd71da7c396a4bf6fb8bc
William <gravilwilliam@gmail.com>
2022-03-21 14:24:02 +0100

    mix-in ?

--- worksheet.py
+++ worksheet.py
@@ -1,5 +1,8 @@
 from modules.open_digraph import *
+from modules.open_digraph_composition import *
 import inspect
+
+from modules.open_digraph_composition import open_digraph_composition
 
 n0 = node(0, 'a', {3:1, 4:1}, {1:1, 2:1})
 n1 = node(1, 'b', {0:1}, {2:2, 5:1})
@@ -8,13 +11,13 @@ i0 = node(3, 'i0', {}, {0:1})
 i1 = node(4, 'i1', {}, {0:1})
 o0 = node(5, 'o0', {1:1}, {})
 o1 = node(6, 'o1', {2:1}, {})
-G = open_digraph([3,4], [5,6], [n0,n1,n2,i0,i1,o0,o1])
+G = open_digraph_composition([3,4], [5,6], [n0,n1,n2,i0,i1,o0,o1])
 
 n3 = node(0, 'x', {}, {1:1,2:1})
 n4 = node(1, 'y', {0:1}, {2:1})
 n5 = node(2, 'z', {0:1,1:1}, {0:1})
 
-opd = open_digraph([0],[2],[n3,n4,n5])
+opd = open_digraph_composition([0],[2],[n3,n4,n5])
 #opd.save_as_dot_file(verbose=True)
 #print(opd.is_cyclic())
 
